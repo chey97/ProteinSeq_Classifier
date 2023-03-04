9e6211834c3b702731b9d092267ea8e9e4e19103
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2023-03-04 18:35:52 +0530

    recheck the code

--- .ProteinSeq_Classifier.py
+++ .ProteinSeq_Classifier.py
@@ -17,7 +17,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 
 df_char = pd.read_csv('data/pdb_data_no_dups.csv')
 df_seq = pd.read_csv('data/pdb_data_seq.csv')
-print(df_seq.head())
+#print(df_seq.head())
 
 # ------- Filter and Process Data -----------------------------------------------------------------------------------------------------------------
 # With the data loaded into two seperate pandas dataframes, a filter, project, and a join must be performed to get the data together.
@@ -29,8 +29,8 @@ protein_seq = df_seq[df_seq.macromoleculeType == 'Protein']
 protein_char = protein_char[['structureId','classification']]
 protein_seq = protein_seq[['structureId','sequence']]
 
-# print(protein_seq.head()) # display the first five rows of the protein_seq dataframe.
-# print(protein_char.head())
+#print(protein_seq.head()) # display the first five rows of the protein_seq dataframe.
+#print(protein_char.head())
 
 model_f = protein_char.set_index('structureId').join(protein_seq.set_index('structureId')) 
 # Joining the two data frames into one with 346,325 proteins
@@ -41,19 +41,19 @@ print('Number of rows in the joined dataset: ', model_f.shape)
 #check NA counts
 model_f.isnull().sum()
 
-# Dropping the rows with missing values (NULL values)
+# Dropping the rows with missing values (NULL values) - this function is included in pandas dataframe 
 model_f = model_f.dropna()
 #print(model_f)
-print('Number of remainning rows: ', model_f.shape)
+print('Number of remainning rows: ', model_f.shape) # This gives that only 4 rows are been consisting null values.
 
 # looking at the types of family groups that clasification can be sorted into.
 
 counts = model_f.classification.value_counts() 
 # count the occurrences of each unique value in the classification column of the model_f dataframe and returns a series object that contains the counts of unique values in descending order.
-print(counts)
+#print(counts)
 
 df = pd.DataFrame({'classification' : counts})
-print(df)
+#print(df)
 df.to_csv('Classification_family_groups.csv')
 
 # --- ploting the counts ---- visualize the distribution of the number of records for each family type
@@ -62,7 +62,7 @@ if not os.path.exists("plots"):
     os.makedirs("plots")
 
 plt.figure()
-sns.displot(data=df, x='classification', kind='kde', color = 'blue')
+sns.displot(data=df, x='classification', kind='ecdf', color = 'blue')
 plt.title('Count Distribution for Family Types')
 plt.ylabel('% of records')
 plt.savefig("plots/Count Distribution for Family Types.png")
@@ -74,8 +74,8 @@ types = np.asarray(counts[(counts > 1000)].index)
 # Filter dataset's records for classification types > 1000
 data = model_f[model_f.classification.isin(types)] #  The 'isin' method checks if each value in the column is contained in the list types
 
-print(types)
-print('%d is the number of records in the final filtered dataset' %data.shape[0])
+#print(types)
+#print('%d is the number of records in the final filtered dataset' %data.shape[0])
 
 # ------ Train Test Split ------------------------------------------------------------------------
 
