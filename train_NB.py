13215005b1943fc491260aa62b59c6d320ea8892
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2023-03-24 19:42:50 +0530

    adding the plots

--- train_NB.py
+++ train_NB.py
@@ -9,12 +9,16 @@ from data_filter import data
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import AdaBoostClassifier
 from tqdm import tqdm
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 # ------ Train Test Split ------------------------------------------------------------------------
 
 # Split Data
-X_train, X_test, y_train, y_test = train_test_split(data['sequence'], data['classification'], test_size = 0.2, random_state = 42)
+#X_train, X_test, y_train, y_test = train_test_split(data['sequence'], data['classification'], test_size = 0.2, random_state = 42)
+
+X_train, X_test, y_train, y_test = train_test_split(data['sequence'], data['classification'], test_size=0.05, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.05, random_state=42)
 
 
 # After splitting the data, it's important to utilize the CountVectorizer to create a dictionary composed from the training dataset. 
@@ -24,11 +28,53 @@ vect = CountVectorizer( analyzer = 'char_wb', ngram_range = (4 , 4) )
 vect.fit(X_train)
 X_train_df = vect.transform(X_train)
 X_test_df = vect.transform(X_test)
+X_val_df = vect.transform(X_val)
 
 # Print a few of the features
 print(vect.get_feature_names_out()[-20:])
+    
+    
+# ------ Machine Learning Model ------
 
-# ------ Machine Learning Models ------
+# Make a prediction dictionary to store accuracies
+prediction = dict()
+
+# set the number of epochs
+epochs = 10
+
+# fit the model for each epoch and update the progress bar
+train_acc, val_acc = [], []
+
+# Set a unique random seed for each epoch
+for epoch in range(epochs):
+    np.random.seed(epoch)
+
+    model = MultinomialNB()
+    model.fit(X_train_df, y_train)
+
+    NB_pred_train = model.predict(X_train_df)
+    acc_train = accuracy_score(NB_pred_train, y_train)
+    train_acc.append(acc_train)
+
+    NB_pred_val = model.predict(X_val_df)
+    acc_val = accuracy_score(NB_pred_val, y_val)
+    val_acc.append(acc_val)
+
+    NB_pred_test = model.predict(X_test_df)
+    acc_test = accuracy_score(NB_pred_test, y_test)
+    prediction["MultinomialNB"] = acc_test
+    print(f"Epoch {epoch+1}: Test Accuracy = {acc_test:.4f}")
+
+# --------------Plot learning curves----------------------
+plt.plot(train_acc, label='Training accuracy')
+plt.plot(val_acc, label='Validation accuracy')
+plt.title('Learning curves')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+'''# ------ Machine Learning Model ------
 
 # Make a prediction dictionary to store accuracies
 prediction = dict()
@@ -43,7 +89,7 @@ for epoch in range(epochs):
     NB_pred = model.predict(X_test_df)
     acc = accuracy_score(NB_pred, y_test)
     prediction["MultinomialNB"] = acc
-    print (acc)
+    print (acc)'''
     
 '''    # update progress bar
     tqdm.write(f"Epoch {epoch+1}/{epochs} - Accuracy: {acc}") 
